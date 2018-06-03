6cd809a6a777881d6a4b1a761fdde68681bfca74
Jakub Zawadzki <jakub.zawadzki@icloud.com>
2018-06-03 02:28:24 +0200

    Edit

--- todolist/tasks/views.py
+++ todolist/tasks/views.py
@@ -1,3 +1,103 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Category, Task
 
-# Create your views here.
+
+def index(request):
+    task = Task.objects.all()
+    return render(request, 'index.html', {
+        'task': task
+    })
+
+
+def categories(request):
+    categories = Category.objects.all()
+    return render(request, 'categories.html', {
+        'categories': categories
+    })
+
+
+def add_category(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        color = request.POST.get('color')
+        if color and name:
+            category = Category(
+                name=name,
+                color=color
+            )
+            category.save()
+    return render(request, 'add_category.html')
+
+
+def edit_category(request, category_id):
+    category = Category.objects.get(id=category_id)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        color = request.POST.get('color')
+        if color and name:
+            category.name = name
+            category.color = color
+            category.save()
+    return render(request, 'edit_category.html', {
+        'category': category
+    })
+
+
+def delete_category(request, category_id):
+    category = Category.objects.get(id=category_id)
+    category.delete()
+    return redirect('categories-list')
+
+
+def tasks(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html', {
+        'tasks': tasks
+    })
+
+
+def add_tasks(request):
+    categories = Category.objects.all()
+    if request.method == 'GET':
+        return render(request, 'add_task.html', {
+            'categories': categories
+        })
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        category_id = request.POST.get('category_id')
+        if name and description and category_id:
+            task = Task(
+                name=name,
+                description=description,
+                category_id=category_id
+                )
+            task.save()
+            return redirect('tasks-list')
+
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('tasks-list')
+
+
+def edit_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    categories = Category.objects.all()
+    if request.method == 'GET':
+        return render(request, 'edit_task.html', {
+            'task': task,
+            'categories': categories
+        })
+    else:
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        category_id = request.POST.get('category_id')
+        if name and description and category_id:
+            task.name = name
+            task.description = description
+            task.category_id = category_id
+            task.save()
+            return redirect('tasks-list')
--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from django.urls import path
+from tasks.views import index, categories, add_category, edit_category, delete_category, add_tasks, tasks, edit_task, delete_task
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('categories/', categories, name='categories-list'),
+    path('add_category/', add_category, name='add-category'),
+    path('edit_category/<int:category_id>', edit_category, name='edit-category'),
+    path('delete_category/<int:category_id>', delete_category, name='delete-category'),
+    path('tasks/', tasks, name='tasks-list'),
+    path('add_task/', add_tasks, name='add-tasks'),
+    path('edit_task/<int:task_id>', edit_task, name='edit-task'),
+    path('delete_task/<int:task_id>', delete_task, name='delete-task')
+
 ]
